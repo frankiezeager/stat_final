d6869ba55f513bbfde99cf80978060cc7f727d1f
Erik <erik.langenborg@gmail.com>
2016-11-29 18:23:15 -0500

    Filtering median household incomes

--- filter_data.R
+++ filter_data.R
@@ -11,13 +11,15 @@ data <- read_csv("acs_data.csv")
 
 data %>% glimpse
 data %>%
-  mutate(health_insurance_coverage_percent = (B27015_003 + B27015_008 + B27015_013 + B27015_018 + B27015_023) / B27015_001) %>%
+  mutate(health_insurance_coverage_percent = (B27015_003 + B27015_008 + B27015_013 + B27015_018 + B27015_023) / B27015_001) %>% # Health Insurance
   select(-(B27015_001:B27015_026)) %>%
-  mutate(housing_units_with_mortgage_percent = B25081_001 / B25081_002) %>%
+  mutate(housing_units_with_mortgage_percent = B25081_001 / B25081_002) %>% # housing mortgage
   select(-(B25081_001:B25081_008)) %>%
-  mutate(poverty_students_percent = B14006_007 / (B14006_007 + B14006_017)) %>%
+  mutate(poverty_students_percent = B14006_007 / (B14006_007 + B14006_017)) %>% # Student Poverty
   select(-(B14006_001:B14006_021)) %>%
-  select(-(B16009_001:B16009_027)) %>%
+  select(-(B16009_001:B16009_027)) %>% # Remove extra poverty status
+  mutate(income_median = B19013_001) %>% # Median Household income
+  select(-B19013_001, -(B19013A_001:B19013I_001)) %>% # remove extra tables
   glimpse
 
 data %>%
