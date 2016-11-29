a5649c23edcbd896718f710b535ba00089b1f89d
Erik <erik.langenborg@gmail.com>
2016-11-29 18:41:23 -0500

    Filtering more of the income and foodstamps

--- filter_data.R
+++ filter_data.R
@@ -20,6 +20,24 @@ data %>%
   select(-(B16009_001:B16009_027)) %>% # Remove extra poverty status
   mutate(income_median = B19013_001) %>% # Median Household income
   select(-B19013_001, -(B19013A_001:B19013I_001)) %>% # remove extra tables
+  select(-(B19019_001:B19019_008)) %>% # remove extra tables
+  select(-(B19049_001:B19049_005)) %>% # remove extra tables
+  select(-(B25099_001:B25099_003)) %>% # remove extra tables
+  select(-(B25119_001:B25119_003)) %>% # median Household tenure
+  select(-(B10010_001:B10010_003)) %>%
+  select(-B19113_001, -(B19113A_001:B19113I_001)) %>% # 	Median Family Income In The Past 12 Months (In 2013 Inflation-Adjusted Dollars)
+  select(-(B19119_001:B19119_007)) %>% #
+  select(-(B08537_001:B08537_021)) %>% # Missing data
+  select(-(B19121_001:B19121_005)) %>% #
+  select(-(B19125_001:B19125_003)) %>% # More median family income
+  select(-(B11017_001:B11017_003), -(B12007_001:B12007_002), -(B12007A_001:B12007I_002)) %>%
+  mutate(family_income_median = B19301_001) %>%
+  select(-(B19126_001:B19301I_001)) %>%
+  mutate(household_foodstamps_percent = B22003_002 / B22003_001) %>%
+  select(-(B22003_001:B22003_007)) %>%
+  mutate(household_foodstamps_percent = (B08137_004 + B08137_007) / B08137_001) %>%
+  select(-(B08137_001:B08137_021)) %>%
+  select(-(C02003_001:C02003_019)) %>% # Detailed Race
   glimpse
 
 data %>%
