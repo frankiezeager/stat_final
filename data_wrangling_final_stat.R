25520bf8bcc433a8269461dab99d07410f050d89
frankiezeager <frankiezeager@yahoo.com>
2016-11-29 17:46:37 -0500

    changed sums to means for aggregation

--- data_wrangling_final_stat.R
+++ data_wrangling_final_stat.R
@@ -113,39 +113,39 @@ for(level in unique(science$DISADVANTAGED_FLAG)){
 
 reading %>% group_by(DIV_NAME)%>% summarise(reading.AVG_SOL_SCALE_SCORE=mean(AVG_SOL_SCALE_SCORE),
                                             reading.PASS_ADVANCED_RATE=mean(PASS_ADVANCED_RATE), reading.PASS_PROF_RATE=mean(PASS_PROF_RATE), reading.PASS_RATE=mean(PASS_RATE),
-                                            reading.FAIL_RATE=mean(FAIL_RATE), reading.num_female=sum(gender_dummy_F), reading.num_frc_3=sum(federal_race_code_3,na.rm=TRUE), reading.num_frc_4=sum(federal_race_code_4,na.rm=TRUE),
-                                            reading.num_frc_5=sum(federal_race_code_5,na.rm=TRUE), reading.num_frc_NA=sum(federal_race_code_NA,na.rm=TRUE), reading.num_frc_99=sum(federal_race_code_99,na.rm=TRUE), reading.num_frc_2=sum(federal_race_code_2,na.rm=TRUE),
-                                            reading.num_frc_1=sum(federal_race_code_1,na.rm=TRUE),reading.num_frc_6=sum(federal_race_code_6,na.rm=TRUE), reading.disability_flag=sum(disability_flag_Y),reading.lep_flag=sum(lep_flag_Y),
-                                            reading.disadvantaged_flag=sum(disadvantaged_flag_Y))->reading.grouped
+                                            reading.FAIL_RATE=mean(FAIL_RATE), reading.num_female=mean(gender_dummy_F), reading.num_frc_3=mean(federal_race_code_3,na.rm=TRUE), reading.num_frc_4=mean(federal_race_code_4,na.rm=TRUE),
+                                            reading.num_frc_5=mean(federal_race_code_5,na.rm=TRUE), reading.num_frc_NA=mean(federal_race_code_NA,na.rm=TRUE), reading.num_frc_99=mean(federal_race_code_99,na.rm=TRUE), reading.num_frc_2=mean(federal_race_code_2,na.rm=TRUE),
+                                            reading.num_frc_1=mean(federal_race_code_1,na.rm=TRUE),reading.num_frc_6=mean(federal_race_code_6,na.rm=TRUE), reading.disability_flag=mean(disability_flag_Y),reading.lep_flag=mean(lep_flag_Y),
+                                            reading.disadvantaged_flag=mean(disadvantaged_flag_Y))->reading.grouped
 
 
 writing %>% group_by(DIV_NAME)%>% summarise(writing.AVG_SOL_SCALE_SCORE=mean(AVG_SOL_SCALE_SCORE),
                                             writing.PASS_ADVANCED_RATE=mean(PASS_ADVANCED_RATE), writing.PASS_PROF_RATE=mean(PASS_PROF_RATE), writing.PASS_RATE=mean(PASS_RATE),
-                                            writing.FAIL_RATE=mean(FAIL_RATE), writing.num_female=sum(gender_dummy_F), writing.num_frc_3=sum(federal_race_code_3,na.rm=TRUE), writing.num_frc_4=sum(federal_race_code_4,na.rm=TRUE),
-                                            writing.num_frc_5=sum(federal_race_code_5,na.rm=TRUE), writing.num_frc_NA=sum(federal_race_code_NA,na.rm=TRUE), writing.num_frc_99=sum(federal_race_code_99,na.rm=TRUE), writing.num_frc_2=sum(federal_race_code_2,na.rm=TRUE),
-                                            writing.num_frc_1=sum(federal_race_code_1,na.rm=TRUE),writing.num_frc_6=sum(federal_race_code_6,na.rm=TRUE), writing.disability_flag=sum(disability_flag_Y),writing.lep_flag=sum(lep_flag_Y),
-                                            writing.disadvantaged_flag=sum(disadvantaged_flag_Y))->writing.grouped
+                                            writing.FAIL_RATE=mean(FAIL_RATE), writing.num_female=mean(gender_dummy_F), writing.num_frc_3=mean(federal_race_code_3,na.rm=TRUE), writing.num_frc_4=mean(federal_race_code_4,na.rm=TRUE),
+                                            writing.num_frc_5=mean(federal_race_code_5,na.rm=TRUE), writing.num_frc_NA=mean(federal_race_code_NA,na.rm=TRUE), writing.num_frc_99=mean(federal_race_code_99,na.rm=TRUE), writing.num_frc_2=mean(federal_race_code_2,na.rm=TRUE),
+                                            writing.num_frc_1=mean(federal_race_code_1,na.rm=TRUE),writing.num_frc_6=mean(federal_race_code_6,na.rm=TRUE), writing.disability_flag=mean(disability_flag_Y),writing.lep_flag=mean(lep_flag_Y),
+                                            writing.disadvantaged_flag=mean(disadvantaged_flag_Y))->writing.grouped
 
 history %>% group_by(DIV_NAME)%>% summarise(history.AVG_SOL_SCALE_SCORE=mean(AVG_SOL_SCALE_SCORE),
                                             history.PASS_ADVANCED_RATE=mean(PASS_ADVANCED_RATE), history.PASS_PROF_RATE=mean(PASS_PROF_RATE), history.PASS_RATE=mean(PASS_RATE),
-                                            history.FAIL_RATE=mean(FAIL_RATE), history.num_female=sum(gender_dummy_F), history.num_frc_3=sum(federal_race_code_3,na.rm=TRUE), history.num_frc_4=sum(federal_race_code_4,na.rm=TRUE),
-                                            history.num_frc_5=sum(federal_race_code_5,na.rm=TRUE), history.num_frc_NA=sum(federal_race_code_NA,na.rm=TRUE), history.num_frc_99=sum(federal_race_code_99,na.rm=TRUE), history.num_frc_2=sum(federal_race_code_2,na.rm=TRUE),
-                                            history.num_frc_1=sum(federal_race_code_1,na.rm=TRUE),history.num_frc_6=sum(federal_race_code_6,na.rm=TRUE), history.disability_flag=sum(disability_flag_Y),history.lep_flag=sum(lep_flag_Y),
-                                            history.disadvantaged_flag=sum(disadvantaged_flag_Y))->history.grouped
+                                            history.FAIL_RATE=mean(FAIL_RATE), history.num_female=mean(gender_dummy_F), history.num_frc_3=mean(federal_race_code_3,na.rm=TRUE), history.num_frc_4=mean(federal_race_code_4,na.rm=TRUE),
+                                            history.num_frc_5=mean(federal_race_code_5,na.rm=TRUE), history.num_frc_NA=mean(federal_race_code_NA,na.rm=TRUE), history.num_frc_99=mean(federal_race_code_99,na.rm=TRUE), history.num_frc_2=mean(federal_race_code_2,na.rm=TRUE),
+                                            history.num_frc_1=mean(federal_race_code_1,na.rm=TRUE),history.num_frc_6=mean(federal_race_code_6,na.rm=TRUE), history.disability_flag=mean(disability_flag_Y),history.lep_flag=mean(lep_flag_Y),
+                                            history.disadvantaged_flag=mean(disadvantaged_flag_Y))->history.grouped
 
 math %>% group_by(DIV_NAME)%>% summarise(math.AVG_SOL_SCALE_SCORE=mean(AVG_SOL_SCALE_SCORE),
                                             math.PASS_ADVANCED_RATE=mean(PASS_ADVANCED_RATE), math.PASS_PROF_RATE=mean(PASS_PROF_RATE), math.PASS_RATE=mean(PASS_RATE),
-                                            math.FAIL_RATE=mean(FAIL_RATE), math.num_female=sum(gender_dummy_F), math.num_frc_3=sum(federal_race_code_3,na.rm=TRUE), math.num_frc_4=sum(federal_race_code_4,na.rm=TRUE),
-                                            math.num_frc_5=sum(federal_race_code_5,na.rm=TRUE), math.num_frc_NA=sum(federal_race_code_NA,na.rm=TRUE), math.num_frc_99=sum(federal_race_code_99,na.rm=TRUE), math.num_frc_2=sum(federal_race_code_2,na.rm=TRUE),
-                                            math.num_frc_1=sum(federal_race_code_1,na.rm=TRUE),math.num_frc_6=sum(federal_race_code_6,na.rm=TRUE), math.disability_flag=sum(disability_flag_Y),math.lep_flag=sum(lep_flag_Y),
-                                            math.disadvantaged_flag=sum(disadvantaged_flag_Y))->math.grouped
+                                            math.FAIL_RATE=mean(FAIL_RATE), math.num_female=mean(gender_dummy_F), math.num_frc_3=mean(federal_race_code_3,na.rm=TRUE), math.num_frc_4=mean(federal_race_code_4,na.rm=TRUE),
+                                            math.num_frc_5=mean(federal_race_code_5,na.rm=TRUE), math.num_frc_NA=mean(federal_race_code_NA,na.rm=TRUE), math.num_frc_99=mean(federal_race_code_99,na.rm=TRUE), math.num_frc_2=mean(federal_race_code_2,na.rm=TRUE),
+                                            math.num_frc_1=mean(federal_race_code_1,na.rm=TRUE),math.num_frc_6=mean(federal_race_code_6,na.rm=TRUE), math.disability_flag=mean(disability_flag_Y),math.lep_flag=mean(lep_flag_Y),
+                                            math.disadvantaged_flag=mean(disadvantaged_flag_Y))->math.grouped
 
 science %>% group_by(DIV_NAME)%>% summarise(science.AVG_SOL_SCALE_SCORE=mean(AVG_SOL_SCALE_SCORE),
                                             science.PASS_ADVANCED_RATE=mean(PASS_ADVANCED_RATE), science.PASS_PROF_RATE=mean(PASS_PROF_RATE), science.PASS_RATE=mean(PASS_RATE),
-                                            science.FAIL_RATE=mean(FAIL_RATE), science.num_female=sum(gender_dummy_F), science.num_frc_3=sum(federal_race_code_3,na.rm=TRUE), science.num_frc_4=sum(federal_race_code_4,na.rm=TRUE),
-                                            science.num_frc_5=sum(federal_race_code_5,na.rm=TRUE), science.num_frc_NA=sum(federal_race_code_NA,na.rm=TRUE), science.num_frc_99=sum(federal_race_code_99,na.rm=TRUE), science.num_frc_2=sum(federal_race_code_2,na.rm=TRUE),
-                                            science.num_frc_1=sum(federal_race_code_1,na.rm=TRUE),science.num_frc_6=sum(federal_race_code_6,na.rm=TRUE), science.disability_flag=sum(disability_flag_Y),science.lep_flag=sum(lep_flag_Y),
-                                            science.disadvantaged_flag=sum(disadvantaged_flag_Y))->science.grouped
+                                            science.FAIL_RATE=mean(FAIL_RATE), science.num_female=mean(gender_dummy_F), science.num_frc_3=mean(federal_race_code_3,na.rm=TRUE), science.num_frc_4=mean(federal_race_code_4,na.rm=TRUE),
+                                            science.num_frc_5=mean(federal_race_code_5,na.rm=TRUE), science.num_frc_NA=mean(federal_race_code_NA,na.rm=TRUE), science.num_frc_99=mean(federal_race_code_99,na.rm=TRUE), science.num_frc_2=mean(federal_race_code_2,na.rm=TRUE),
+                                            science.num_frc_1=mean(federal_race_code_1,na.rm=TRUE),science.num_frc_6=mean(federal_race_code_6,na.rm=TRUE), science.disability_flag=mean(disability_flag_Y),science.lep_flag=mean(lep_flag_Y),
+                                            science.disadvantaged_flag=mean(disadvantaged_flag_Y))->science.grouped
 
 
 fully_merged<-merge(reading.grouped,writing.grouped, by="DIV_NAME")
