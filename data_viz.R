9a851c4f7e95d01774270af300de2ca6daf2de84
Tyler <tch6zf@virginia.edu>
2016-12-05 12:35:01 -0500

    updated code

--- data_viz.R
+++ data_viz.R
@@ -4,9 +4,9 @@
 
 full_data<-read.csv('data_merge.csv')
 
-# reassign family income variable and house worth
-full_data$family_income_median_adj<-full_data$family_income_median/1000
-full_data$house_worth_dollars_adj <- full_data$house_worth_dollars/1000000
+# Reassign family income variable and house worth
+full_data$family_income_median_adj<-full_data$family_income_median/1000  #scale to thousands
+full_data$house_worth_dollars_adj <- full_data$house_worth_dollars/1000000  #scale to millions
 
 ##########################################################################
 # Some scatter plots
@@ -15,33 +15,25 @@ library(gridExtra)
 library(dplyr)
 library(purrr)
 
-income.read <- ggplot(full_data, aes(x = family_income_median_adj, y = Reading.SOL))
-# p1 <- income.read +geom_point()
-# p1
-income.read <- income.read + geom_point(aes(size=house_worth_dollars_adj))
-income.read #this is neat
+theme <- theme(                              
+  axis.title.x = element_text(face="bold", color="black", size=10),
+  axis.title.y = element_text(face="bold", color="black", size=10),
+  plot.title = element_text(face="bold", color = "black", size=12)
+)
 
-partner.read <- ggplot(full_data, aes(x = unmarried_partner_present_percent, y = Reading.SOL))
-# p3 <- partner.read + geom_point()
-# p3
-partner.read <- partner.read + geom_point(aes(size=family_income_median_adj))
+income.read <- ggplot(full_data, aes(x = family_income_median_adj, y = Reading.SOL)) +
+  geom_point(aes(size=house_worth_dollars_adj)) +
+  scale_size_continuous(name ="House Worth (in millions $)", range=c(0.5,6)) +
+  theme_bw() + theme +
+  labs(title="Reading SOL Scores vs Median Income", x="Median Family Income", y="Reading SOL Scores") 
+income.read 
+
+partner.read <- ggplot(full_data, aes(x = unmarried_partner_present_percent, y = Reading.SOL)) + 
+  geom_point(aes(size=family_income_median_adj)) +
+  scale_size_continuous(name ="Median Family Income", range=c(0.2,4)) +
+  theme_bw() + theme +
+  labs(title="Reading SOL Scores vs Presence of Unmarried Partner", x="% of Homes with Unmarried Partner Present (by county)", y="Reading SOL Scores") 
 partner.read
-
-#income.write <- ggplot(full_data, aes(x = family_income_median_adj, y = Writing.SOL))
-#p5 <- income.write + geom_point(aes(size=house_worth_dollars_adj))
-#p5 
-
-#partner.write <- ggplot(full_data, aes(x = unmarried_partner_present_percent, y = Writing.SOL))
-#p6 <- partner.write + geom_point(aes(size=family_income_median_adj))
-#p6
-
-#income.history <- ggplot(full_data, aes(x = family_income_median_adj, y = History.SOL))
-#p7 <- income.history + geom_point(aes(size=house_worth_dollars_adj))
-#p7 
-
-#partner.history <- ggplot(full_data, aes(x = unmarried_partner_present_percent, y = History.SOL))
-#p8 <- partner.history + geom_point(aes(size=family_income_median_adj))
-#p8
 
 income.math <- ggplot(full_data, aes(x = family_income_median_adj, y = Math.SOL))
 income.math <- income.math + geom_point(aes(size=house_worth_dollars_adj))
@@ -50,23 +42,6 @@ income.math
 partner.math <- ggplot(full_data, aes(x = unmarried_partner_present_percent, y = Math.SOL))
 partner.math <- partner.math + geom_point(aes(size=family_income_median_adj))
 partner.math
-
-#income.science <- ggplot(full_data, aes(x = family_income_median_adj, y = Science.SOL))
-#p11 <- income.science + geom_point(aes(size=house_worth_dollars_adj))
-#p11
-
-#partner.science <- ggplot(full_data, aes(x = unmarried_partner_present_percent, y = Science.SOL))
-#p12 <- partner.science + geom_point(aes(size=family_income_median_adj))
-#p12
-##########################
-#read.write <- ggplot(full_data, aes(x=Reading.SOL, y=Writing.SOL))
-#t1 <- read.write + geom_point()
-#t1  
-
-#math.science <- ggplot(full_data, aes(x=Math.SOL, y=Science.SOL))
-#t2 <- math.science + geom_point()
-#t2 <- t2 + geom_point(aes(size=house_worth_dollars_adj))
-#t2
 
 read.math <- ggplot(full_data, aes(x=Math.SOL, y=Reading.SOL)) + geom_point(aes(size=house_worth_dollars_adj))
 read.math
@@ -77,6 +52,7 @@ library(maptools)
 library(ggmap)
 library(RColorBrewer)
 library(scales)
+library(Cairo)
 set.seed(12345)
 setwd("tl_2014_51_unsd/")
 
@@ -95,19 +71,23 @@ virginia.shp <- virginia.shp[order(virginia.shp$order),]
 va.reading <- ggplot() + 
   geom_polygon(data = virginia.shp, aes(x = long, y = lat, group = group, fill = Reading.SOL), color= "black", size = 0.25) + 
   coord_map() +
-  labs(title="Reading SOL Scores in Virginia") +
   scale_fill_distiller(name="Reading SOL", palette = "YlGn", breaks = pretty_breaks(n = 5), trans="reverse")+
-  theme_nothing(legend = TRUE)
+  theme_nothing(legend = TRUE) +
+  ggtitle("Reading SOL Scores in Virginia") + 
+  theme(plot.title = element_text(size = 12, face="bold"))
 va.math <- ggplot() + 
   geom_polygon(data = virginia.shp, aes(x = long, y = lat, group = group, fill = Math.SOL), color= "black", size = 0.25) + 
   coord_map() +
-  labs(title="Math SOL Scores in Virginia") +
   scale_fill_distiller(name="Math SOL", palette = "YlGn", breaks = pretty_breaks(n = 5), trans="reverse")+
-  theme_nothing(legend = TRUE)
+  theme_nothing(legend = TRUE) +
+  ggtitle("Math SOL Scores in Virginia") + 
+  theme(plot.title = element_text(size = 12, face="bold"))
 
-grid.arrange(va.reading, va.math, nrow=2)
-
-
+# Save the plots
+path <- "~/Desktop/Data_Science/Linear_Models/stat_final"
+setwd(path)
+ggsave(va.reading, file = "ReadingSOL.png", width = 6, height = 4.5, type = "cairo-png")
+ggsave(va.math, file = "MathSOL.png", width = 6, height = 4.5, type = "cairo-png")
 
 
 
