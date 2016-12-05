4118821dde1f3fc56485c22e169c3a83641b65f2
Erik <erik.langenborg@gmail.com>
2016-12-04 19:31:33 -0500

    Added missing dependencies

--- acs.init.R
+++ acs.init.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("acs", "dplyr", "purrr", "dtplyr", "readr", "mice")
+list.of.packages <- c("acs", "dplyr", "purrr", "readr", "mice", "gridExtra", "ggplot2", "car", "maptools", "ggmap", "gpclib")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
